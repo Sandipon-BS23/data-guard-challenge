73e3318f717b3c749385a41c769694fe2f85d2b1
Sandipon-BS23 <Sandipon.Paul@brainstation-23.com>
2022-05-04 15:09:26 +0600

    Checking Mock server on heroku

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,13 +4,13 @@
     </div>
 </template>
 <script setup lang="ts">
-import axios from 'src/lib/axios'
-import { onMounted } from 'vue'
-// onMounted(() => {
-axios.get('/todos').then((response) => {
-    console.log('response:', response)
-})
-console.log(`the component is now mounted.`)
+// import axios from 'src/lib/axios'
+// import { onMounted } from 'vue'
+// // onMounted(() => {
+// axios.get('/todos').then((response) => {
+//     console.log('response:', response)
 // })
+// console.log(`the component is now mounted.`)
+// // })
 </script>
 <style lang=""></style>
