c4b11330474759d360b02423592ef4275defde11
Sandipon-BS23 <Sandipon.Paul@brainstation-23.com>
2022-05-04 15:04:17 +0600

    Checking Mock server on heroku

--- src/views/finance.vue
+++ src/views/finance.vue
@@ -2,13 +2,16 @@
     <div>
         <h1>Finance</h1>
         <!-- {{ axios }} -->
+        {{ fakeData }}
     </div>
 </template>
 <script setup lang="ts">
 import axios from 'axios'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+const fakeData = ref([])
 axios.get('/api/todos').then((response) => {
     console.log('response:', response)
+    fakeData.value = response.data
 })
 onMounted(() => {
     console.log(`the component is now mounted.`)
